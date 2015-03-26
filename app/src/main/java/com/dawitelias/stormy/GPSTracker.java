63c8f3d380231fba6884dfa50d3c3967977fde99
dawitelias <dawitelias14@gmail.com>
2015-03-26 11:07:55 -0400

    Initial commit

---
+++ app/src/main/java/com/dawitelias/stormy/GPSTracker.java
@@ -0,0 +1,295 @@
+package com.dawitelias.stormy;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by dxe on 3/24/2015.
+ */
+public class GPSTracker extends Service implements LocationListener {
+
+    public static final String TAG = GPSTracker.class.getSimpleName();
+
+    private final Context mContext;
+
+    boolean isGPSEnabled = false; // flag for GPS status
+    boolean isNetworkEnabled = false; // flag for network status
+    boolean canGetLocation = false;
+
+    Location location;
+    double latitude, longitude;
+
+    // Minimum distance to change updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; //10 meters
+
+    // Minimum time betwen updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minute
+
+    protected LocationManager locationManager;
+
+    public GPSTracker(Context context) {
+        this.mContext = context;
+        getLocation();
+    }
+
+
+    public Location getLocation() {
+
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            // get GPS status
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // get network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network or gps
+                this.canGetLocation = false;
+            } else {
+                this.canGetLocation = true;
+
+                // get location from  network provider
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                    Log.d(TAG, "Network up!");
+
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        updateGPSCoordinates();
+                    }
+                } // is network enabled
+
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                        Log.d(TAG, "GPS up!");
+
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            updateGPSCoordinates();
+                        }
+                    }
+                }
+            } // else
+        } catch (Exception e) {
+            Log.e("Error : Location", "Impossible to connect to LocationManager", e);
+        }
+
+        return location;
+    }
+
+    public void updateGPSCoordinates() {
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+    }
+
+    /**
+     * Stop using GPS listener
+     * Calling this function will stop using GPS in your app
+     */
+    public void stopGPSPolling() {
+        if (locationManager != null) {
+            locationManager.removeUpdates(GPSTracker.this);
+        }
+    }
+
+    public double getLatitude() {
+        if (location != null) {
+            latitude = location.getLatitude();
+        }
+        return latitude;
+    }
+
+    public double getLongitude() {
+        if (location != null) {
+            longitude = location.getLongitude();
+        }
+        return longitude;
+    }
+
+    /**
+     * Function to check GPS/wifi enabled
+     */
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    /**
+     * Get list of address by latitude and longitude
+     * @return null or List<Address>
+     */
+    public List<Address> getGeocoderAddress(Context context)
+    {
+        if (location != null)
+        {
+            Geocoder geocoder = new Geocoder(context, Locale.ENGLISH);
+            try {
+                List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                return addresses;
+            } catch (IOException e) {
+                //e.printStackTrace();
+                Log.e("Error : Geocoder", "Impossible to connect to Geocoder", e);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Try to get CountryName
+     * @return null or postalCode
+     */
+    public String getCountryName(Context context) {
+        List<Address> addresses = getGeocoderAddress(context);
+        if (addresses != null && addresses.size() > 0) {
+            Address address = addresses.get(0);
+            String countryName = address.getCountryName();
+
+            return countryName;
+
+        } else {
+            return "--";
+        }
+    }
+
+    /**
+     * Try to get Locality
+     * @return null or locality
+     */
+    public String getLocality(Context context) {
+        List<Address> addresses = getGeocoderAddress(context);
+        if (addresses != null && addresses.size() > 0) {
+            Address address = addresses.get(0);
+            String locality = address.getLocality();
+
+            return locality;
+        } else {
+            return "--";
+        }
+    }
+
+    /**
+     * Try to get Locality
+     * @return null or locality
+     */
+    public String getAdminArea(Context context) {
+        List<Address> addresses = getGeocoderAddress(context);
+        if (addresses != null && addresses.size() > 0) {
+            Address address = addresses.get(0);
+            String adminArea = address.getAdminArea();
+
+            return adminArea;
+        } else {
+            return "--";
+        }
+    }
+
+
+
+    /**
+     * Try to get Postal Code
+     * @return null or postalCode
+     */
+    public String getPostalCode(Context context)
+    {
+        List<Address> addresses = getGeocoderAddress(context);
+        if (addresses != null && addresses.size() > 0)
+        {
+            Address address = addresses.get(0);
+            String postalCode = address.getPostalCode();
+
+            return postalCode;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /**
+     * Function to show settings alert dialog
+     */
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        //Setting Dialog Title
+        alertDialog.setTitle("Network/GPS Off");
+
+        //Setting Dialog Message
+        alertDialog.setMessage("Your location is turned off. We recommend you turn it on to get the most accurate weather data.");
+
+        //On Pressing Setting button
+        alertDialog.setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        //On pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+
+}
